4779474f78f068bb82ee180955020bee1402fbe0
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-18 14:54:43 -0600

    tried to get a redirect to work on form submit, but couldnt get it working. I'll get it eventually!

--- client/src/pages/AddQuote/AddQuote.js
+++ client/src/pages/AddQuote/AddQuote.js
@@ -1,44 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { Input, FormBtn, UploadBtn } from '../../components/Form/index'
-import API from "../../utils/API"
+import API from "../../utils/API";
 import "./AddQuote.css";
+import { Link, withRouter, Redirect } from 'react-router-dom';
+// import history from '../../utils/history';
+// import { push } from 'connected-react-router';
+// import { useDispatch } from 'react-redux'
+// import { withRouter } from 'react-router-dom';
 // find a way of getting the user ID for the API.savequote functionality //if we have time, get it from the profile page
 
 
 const AddQuotes = () => {
 
-  const [formObject, setFormObject] = useState({})
+  const [formObject, setFormObject] = useState({});
 
   function handleInputChange(event) {
     const { name, value } = event.target;
     setFormObject({...formObject, [name]: value})
   };
 
-  // function awsPost() {
-  //   handleUpload = (ev)
-  // } //this isnt working bc of jquery
-
   function handleFormSubmit(event) {
     event.preventDefault();
     if (formObject.quote) {
-      // console.log("cheese")
       API.saveQuote({
         person: formObject.person,
         quote: formObject.quote
       })
-      // .then(awsPost)
       .catch(err => console.log(err));
     }
   };
 
-  // function handleRedirect () {
-  //   return(<Redirect to ={{pathname: "/home"}} />)
-  // }
 
   return (
     <div className="new-quote-form">
       <form>
-        {/* maybe add something to the navbar to hold the username */}
         <Input 
           onChange={handleInputChange}
           name="person"
@@ -54,6 +49,7 @@ const AddQuotes = () => {
         <FormBtn //this is the submit button
           disable={!(formObject.title || formObject.description)}
           onClick={handleFormSubmit}>
+            {/* I need this to redirect, but idk how :/ */}
         </FormBtn>
       </form>
     </div>
@@ -61,4 +57,4 @@ const AddQuotes = () => {
     
 }
 
-export default AddQuotes;
+export default withRouter(AddQuotes);
